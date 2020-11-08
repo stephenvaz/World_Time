ea0f2ee1dd859e2a4ef5c1f894d0ec86c8e6081b
Atharv56 <69430707+Atharv56@users.noreply.github.com>
2020-11-09 00:00:10 +0530

    flags today

--- world_time_app/ios/Flutter/flutter_export_environment.sh
+++ world_time_app/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Users\Stephen\Dev\Git\Flutter\world_time_app"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Github codes\World_Time\world_time_app"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=D:\src\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
