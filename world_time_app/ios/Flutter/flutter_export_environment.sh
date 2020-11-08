4554f6b033c396072bd35041a0ffdf5c5f518584
Udit786 <69597210+Udit786@users.noreply.github.com>
2020-11-09 00:15:50 +0530

    Update flutter_export_environment.sh

--- world_time_app/ios/Flutter/flutter_export_environment.sh
+++ world_time_app/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
+<<<<<<< Updated upstream
 export "FLUTTER_ROOT=C:\src\flutter"
 export "FLUTTER_APPLICATION_PATH=D:\Github codes\World_Time\world_time_app"
+=======
+export "FLUTTER_ROOT=D:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\TEMP.DESKTOP-FEK3EAD.000\Documents\GitHub\World_Time\world_time_app"
+>>>>>>> Stashed changes
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
